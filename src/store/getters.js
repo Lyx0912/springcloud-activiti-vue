a4490a39a54378bd33a6d9c14fccd4d6f56f88ec
lyx <1677685900@qq.com>
2022-11-03 18:02:01 +0800

    init

---
+++ src/store/getters.js
@@ -0,0 +1,10 @@
+const getters = {
+  sidebar: state => state.app.sidebar,
+  device: state => state.app.device,
+  token: state => state.user.token,
+  avatar: state => state.user.avatar,
+  name: state => state.user.name,
+  permissions: state => state.user.permissions,
+  visitedViews: state => this.state.tagsView.visitedViews
+}
+export default getters
